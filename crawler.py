f2b5a8a804458d46fd62d3281777664b3cd96004
MatheusDevelop <mathvierabarbosa@gmail.com>
2023-10-19 10:27:12 -0300

    fix: refactor

--- crawler.py
+++ crawler.py
@@ -1,57 +1,68 @@
-from selenium.webdriver import Chrome 
-from selenium.webdriver import ChromeOptions 
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 import requests
-chrome_options = ChromeOptions()
-chrome_options.add_argument('--headless')
 
-driver = Chrome(options=chrome_options)
+def sync_data_with_api(url, data):
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()
+        return response.status_code
+    except requests.exceptions.RequestException as e:
+        print("Falha na sincronização.")
+        print("Erro:", e)
+        return None
 
-apiUrl = "http://localhost:8080/documents/sync"
-url = "https://www.vriconsulting.com.br/guias/guiasIndex.php?idGuia=22"
-print("Navegando ao site...")
-driver.get(url)
-print("Sincronizando...")
+def extract_table_data(driver):
+    tabela = driver.find_element(By.XPATH, '//*[@id="corpoGuia"]/table[1]/tbody')
+    linhas = tabela.find_elements(By.TAG_NAME, "tr")
+    dados_da_tabela = []
 
-tabela = driver.find_element(By.XPATH, '//*[@id="corpoGuia"]/table[1]/tbody')
+    for linha in linhas:
+        cells = linha.find_elements(By.TAG_NAME, "td")
 
-linhas = tabela.find_elements(By.TAG_NAME, "tr")
+        if len(cells) == 8:
+            n, campo, descricao, tipo, tamanho, decimais, entrada, saida = [cell.text for cell in cells]
 
-dados_da_tabela = []
-print("Sincronizando tabela")
-for linha in linhas:
-    cells = linha.find_elements(By.TAG_NAME, "td")
+            dado = {
+                "Numero": n,
+                "Campo": campo,
+                "Descricao": descricao,
+                "Tipo": tipo,
+                "Tamanho": tamanho,
+                "Decimais": decimais,
+                "Entrada": entrada,
+                "Saida": saida
+            }
+
+            dados_da_tabela.append(dado)
+
+    return dados_da_tabela
+
+def main():
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--log-level=3');
     
-    if len(cells) == 8:
-        n = cells[0].text
-        campo = cells[1].text
-        descricao = cells[2].text
-        tipo = cells[3].text
-        tamanho = cells[4].text
-        decimais = cells[5].text
-        entrada = cells[6].text
-        saida = cells[7].text
-        
-        dado = {
-            "Numero": n,
-            "Campo": campo,
-            "Descricao": descricao,
-            "Tipo": tipo,
-            "Tamanho": tamanho,
-            "Decimais": decimais,
-            "Entrada": entrada,
-            "Saida":saida
-        }
-        
-        dados_da_tabela.append(dado)
+    driver = Chrome(options=chrome_options)
+    url = "https://www.vriconsulting.com.br/guias/guiasIndex.php?idGuia=22"
+    print("Navegando ao site...")
+    driver.get(url)
+    print("Sincronizando...")
 
-driver.quit()
-print("Sincronizando com banco de dados...")
-response = requests.post(apiUrl,json= dados_da_tabela)
-if response.status_code == 201:
-    print("Sincronização bem-sucedida.")
-    print("Você já pode fechar esse programa.")
-else:
-    print("Falha na sincronização.")
-    print("Código de status:", response.status_code)
-    print("Resposta do servidor:", response.text)
+    dados_da_tabela = extract_table_data(driver)
+    driver.quit()
+
+    api_url = "http://localhost:8080/documents/sync"
+    print("Sincronizando com banco de dados...")
+    status_code = sync_data_with_api(api_url, dados_da_tabela)
+
+    if status_code == 201:
+        print("Sincronização bem-sucedida.")
+        print("Você já pode fechar esse programa.")
+    elif status_code is not None:
+        print("Falha na sincronização.")
+        print("Código de status:", status_code)
+
+if __name__ == "__main__":
+    main()
